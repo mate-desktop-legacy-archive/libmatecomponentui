c92a63b1fea63ad53e2e4fe1ec5bbff85c9be995
Stefano Karapetsas <stefano@karapetsas.com>
2011-12-07 00:15:06 +0100

    fixed autogen.sh, removed unused files

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="libmatecomponentui"
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/libmatecomponentui.h) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
